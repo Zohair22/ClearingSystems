4d4042259002d71954c4b06e042d5f266f96e046
zohairosama <zohairosama321@gmail.com>
2021-06-22 22:58:10 +0200

    Add existing file

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card bg-success">
-                    <div class="card-header">{{ __('Register') }}</div>
+                <div class="card bg-success font-weight-bold">
+                    <div class="card-header">{{ __('Add new User') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('addNewUser') }}">
